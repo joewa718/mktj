d4e8a5f2dd6bf9815d927c46797e053c73bd8f6c
Wang Zhan <Wang.Zhan.ap@nielsen.com>
2017-08-09 11:42:01 +0800

    init

--- src/main/java/com/mktj/cn/web/repositories/UserRepository.java
+++ src/main/java/com/mktj/cn/web/repositories/UserRepository.java
@@ -1,18 +1,9 @@
 package com.mktj.cn.web.repositories;
-
 import com.mktj.cn.web.po.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-
 @Repository
 public interface UserRepository extends CrudRepository<User, Long>, JpaSpecificationExecutor<User> {
     User findOne(Long id);
