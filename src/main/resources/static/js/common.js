6f768ef1b8af6efbcebbd7610a3000a1fd37114b
Wang Zhan <Wang.Zhan.ap@nielsen.com>
2017-08-13 21:09:14 +0800

    init

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -2,7 +2,7 @@
  * Common js
  */
 domainUrl="http://whstudy.github.io/htdocs/";
-serviceUrl="http://122.152.208.113";
+serviceUrl="";
 imageNginxUrl="http://122.152.208.113:8080/";
 
 function getQueryString(name) {
