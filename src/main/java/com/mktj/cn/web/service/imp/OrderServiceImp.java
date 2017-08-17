451f19647e0eba7dea6e97985c2a49359f7f2b41
Wang Zhan <Wang.Zhan.ap@nielsen.com>
2017-08-17 16:00:34 +0800

    commit

--- src/main/java/com/mktj/cn/web/service/imp/OrderServiceImp.java
+++ src/main/java/com/mktj/cn/web/service/imp/OrderServiceImp.java
@@ -57,19 +57,10 @@ public class OrderServiceImp extends BaseService implements OrderService {
         if (deliveryAddress == null) {
             throw new OperationNotSupportedException("无法找到对应的收货地址");
         }
-        if (product.getRoleType() != null && product.getRoleType().getCode() > 0) {
-            transactionPackageOrder(user, orderVo, deliveryAddress, product);
-        } else {
-            if (orderVo.getProductNum() == 0) {
-                throw new OperationNotSupportedException("购买订单量不能为 0 件");
-            }
-            transactionOrdinaryOrder(user, orderVo, deliveryAddress, product);
+        if (orderVo.getProductNum() == 0) {
+            throw new OperationNotSupportedException("购买订单量不能为 0 件");
         }
-    }
 
-
-    @Transactional(value = "transactionManager", propagation = Propagation.REQUIRED)
-    private void transactionOrdinaryOrder(User user, OrderVo orderVo, DeliveryAddress deliveryAddress, Product product) {
         int piece = orderVo.getProductNum();
         BigDecimal price = getProductPrice(user.getRoleType(), product);
         BigDecimal totalCost = price.multiply(BigDecimal.valueOf(piece));
@@ -79,41 +70,15 @@ public class OrderServiceImp extends BaseService implements OrderService {
             }
             user.setScore(user.getScore().subtract(totalCost));
         }
+
         saveOrder(orderVo, user, product, deliveryAddress, piece, price, totalCost);
         user.getOrderAnalysis().setUnPay(user.getOrderAnalysis().getAlPay() + 1);
         userRepository.save(user);
     }
 
-    @Transactional(value = "transactionManager", propagation = Propagation.REQUIRED)
-    private void transactionPackageOrder(User user, OrderVo orderVo, DeliveryAddress deliveryAddress, Product product) {
-        User recommend_man = userRepository.findByPhone(orderVo.getRecommendPhone());
-        if (recommend_man == null) {
-            throw new RuntimeException("您填写的推荐人手机号码不存在");
-        }
-        int piece = product.getPiece();
-        BigDecimal price = product.getRetailPrice();
-        BigDecimal totalCost = price.multiply(BigDecimal.valueOf(piece));
-        if (orderVo.getPayType() == PayType.余额支付) {
-            if (totalCost.compareTo(user.getScore()) == 1) {
-                throw new RuntimeException("对不起，您的积分不足，无法购买");
-            }
-            user.setScore(user.getScore().subtract(totalCost));
-        }
-        //更新订单信息
-        Order order = saveOrder(orderVo, user, product, deliveryAddress, piece, price, totalCost);
-        user.setRoleType(product.getRoleType());
-        user.setAuthorizationCode(generateAuthCode());
-        user.getOrderAnalysis().setUnPay(user.getOrderAnalysis().getAlPay() + 1);
-        userRepository.save(user);
-        //更新推荐人服务订单
-        order.getHigherUserList().add(recommend_man);
-        recommend_man.getServiceOrderAnalysis().setUnPay(recommend_man.getServiceOrderAnalysis().getUnPay() + 1);
-        recommend_man.getServiceOrderList().add(order);
-        userRepository.save(recommend_man);
-    }
 
     @Transactional(value = "transactionManager", propagation = Propagation.REQUIRED)
-    private Order saveOrder(OrderVo orderVo, User user, Product product, DeliveryAddress deliveryAddress, int piece, BigDecimal price, BigDecimal totalCost) {
+    protected Order saveOrder(OrderVo orderVo, User user, Product product, DeliveryAddress deliveryAddress, int piece, BigDecimal price, BigDecimal totalCost) {
         Order order = new Order();
         order.setOrderCode(generateOrderCode(String.valueOf(user.getId())));
         order.setUser(user);
@@ -141,79 +106,6 @@ public class OrderServiceImp extends BaseService implements OrderService {
         order.setUser(user);
         return orderRepository.save(order);
     }
-
-
-    @Transactional(value = "transactionManager", propagation = Propagation.REQUIRED)
-    public void updateHigherLevel(User recommend_man, User user, Product product, Order order, int level) {
-        if (level == 3) {
-            return;
-        }
-        User curUser = recommend_man;
-        if (recommend_man == null) {
-            curUser = user;
-        }
-        curUser.getServiceOrderAnalysis().setUnPay(user.getServiceOrderAnalysis().getAlPay() + 1);
-        if (product.getRoleType().getCode() > user.getRoleType().getCode()) {
-            calTeamAnalysis(user, product, curUser);
-        }
-        List<TeamOrganization> teamOrganizationList = curUser.getLowerList().stream().filter(teamOrganization -> teamOrganization.getLowerUser().getId() == user.getId()).collect(Collectors.toList());
-        if (teamOrganizationList.size() == 0) {
-            TeamOrganization teamOrganization = new TeamOrganization();
-            teamOrganization.setHigherUser(curUser);
-            teamOrganization.setLowerUser(user);
-            teamOrganization.setHigherUser(recommend_man);
-            curUser.getLowerList().add(teamOrganization);
-        }
-        curUser.getServiceOrderList().add(order);
-        order.getHigherUserList().add(curUser);
-        userRepository.save(curUser);
-        List<TeamOrganization> userList = curUser.getHigherUserList();
-        for (TeamOrganization higherUser : userList) {
-            updateHigherLevel(higherUser.getHigherUser(), user, product, order, ++level);
-        }
-    }
-
-    /**
-     * 计算团队人员成分
-     *
-     * @param user
-     * @param product
-     * @param sendUser
-     */
-    private void calTeamAnalysis(User user, Product product, User sendUser) {
-        if (user.getRoleType() == RoleType.天使) {
-            sendUser.getTeamAnalysis().setAngle(sendUser.getTeamAnalysis().getAngle() - 1);
-        }
-
-        if (user.getRoleType() == RoleType.合伙人) {
-            sendUser.getTeamAnalysis().setPartner(sendUser.getTeamAnalysis().getPartner() - 1);
-        }
-
-        if (user.getRoleType() == RoleType.准合伙人) {
-            sendUser.getTeamAnalysis().setQuasiPartner(sendUser.getTeamAnalysis().getQuasiPartner() - 1);
-        }
-
-        if (user.getRoleType() == RoleType.高级合伙人) {
-            sendUser.getTeamAnalysis().setSeniorPartner(sendUser.getTeamAnalysis().getSeniorPartner() - 1);
-        }
-
-        if (product.getRoleType() == RoleType.天使) {
-            sendUser.getTeamAnalysis().setAngle(sendUser.getTeamAnalysis().getAngle() + 1);
-        }
-
-        if (product.getRoleType() == RoleType.合伙人) {
-            sendUser.getTeamAnalysis().setPartner(sendUser.getTeamAnalysis().getPartner() + 1);
-        }
-
-        if (product.getRoleType() == RoleType.准合伙人) {
-            sendUser.getTeamAnalysis().setQuasiPartner(sendUser.getTeamAnalysis().getQuasiPartner() + 1);
-        }
-
-        if (product.getRoleType() == RoleType.高级合伙人) {
-            sendUser.getTeamAnalysis().setSeniorPartner(sendUser.getTeamAnalysis().getSeniorPartner() + 1);
-        }
-    }
-
 
     public BigDecimal getProductPrice(RoleType roleType, Product product) {
         if (roleType == RoleType.天使 && product.getPrice1() != null) {
---
+++ src/main/java/com/mktj/cn/web/service/imp/ServiceOrderServiceImp.java
@@ -0,0 +1,154 @@
+package com.mktj.cn.web.service.imp;
+
+import com.mktj.cn.web.dto.EntryDTO;
+import com.mktj.cn.web.dto.OrderDTO;
+import com.mktj.cn.web.mapper.OrderMapper;
+import com.mktj.cn.web.po.*;
+import com.mktj.cn.web.repositories.DeliveryAddressRepository;
+import com.mktj.cn.web.repositories.OrderRepository;
+import com.mktj.cn.web.repositories.ProductRepository;
+import com.mktj.cn.web.repositories.UserRepository;
+import com.mktj.cn.web.service.BaseService;
+import com.mktj.cn.web.service.OrderService;
+import com.mktj.cn.web.util.OrderStatus;
+import com.mktj.cn.web.util.OrderType;
+import com.mktj.cn.web.util.PayType;
+import com.mktj.cn.web.util.RoleType;
+import com.mktj.cn.web.vo.OrderVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.naming.OperationNotSupportedException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * @author zhanwang
+ * @create 2017-08-09 13:27
+ **/
+@Service
+public class ServiceOrderServiceImp extends OrderServiceImp{
+    @Autowired
+    DeliveryAddressRepository deliveryAddressRepository;
+    @Autowired
+    ProductRepository productRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @Transactional(value = "transactionManager")
+    @Override
+    public void transactionOrder(String phone, OrderVo orderVo) throws OperationNotSupportedException {
+        User user = userRepository.findByPhone(phone);
+        Product product = productRepository.findOne(orderVo.getProductId());
+        if (product == null) {
+            throw new OperationNotSupportedException("无法找到对应的商品");
+        }
+        DeliveryAddress deliveryAddress = deliveryAddressRepository.findOneByIdAndUser(orderVo.getDeliverAddressId(), user);
+        if (deliveryAddress == null) {
+            throw new OperationNotSupportedException("无法找到对应的收货地址");
+        }
+        User recommend_man = userRepository.findByPhone(orderVo.getRecommendPhone());
+        if (recommend_man == null) {
+            throw new RuntimeException("您填写的推荐人手机号码不存在");
+        }
+        int piece = product.getPiece();
+        BigDecimal price = product.getRetailPrice();
+        BigDecimal totalCost = price.multiply(BigDecimal.valueOf(piece));
+        if (orderVo.getPayType() == PayType.余额支付) {
+            if (totalCost.compareTo(user.getScore()) == 1) {
+                throw new RuntimeException("对不起，您的积分不足，无法购买");
+            }
+            user.setScore(user.getScore().subtract(totalCost));
+        }
+        //更新订单信息
+        Order order = super.saveOrder(orderVo, user, product, deliveryAddress, piece, price, totalCost);
+        user.setRoleType(product.getRoleType());
+        user.setAuthorizationCode(generateAuthCode());
+        user.getOrderAnalysis().setUnPay(user.getOrderAnalysis().getAlPay() + 1);
+        userRepository.save(user);
+        //更新推荐人服务订单
+        order.getHigherUserList().add(recommend_man);
+        recommend_man.getServiceOrderAnalysis().setUnPay(recommend_man.getServiceOrderAnalysis().getUnPay() + 1);
+        recommend_man.getServiceOrderList().add(order);
+        userRepository.save(recommend_man);
+    }
+
+
+
+    @Transactional(value = "transactionManager", propagation = Propagation.REQUIRED)
+    public void updateHigherLevel(User recommend_man, User user, Product product, Order order, int level) {
+        if (level == 3) {
+            return;
+        }
+        User curUser = recommend_man;
+        if (recommend_man == null) {
+            curUser = user;
+        }
+        curUser.getServiceOrderAnalysis().setUnPay(user.getServiceOrderAnalysis().getAlPay() + 1);
+        if (product.getRoleType().getCode() > user.getRoleType().getCode()) {
+            calTeamAnalysis(user, product, curUser);
+        }
+        List<TeamOrganization> teamOrganizationList = curUser.getLowerList().stream().filter(teamOrganization -> teamOrganization.getLowerUser().getId() == user.getId()).collect(Collectors.toList());
+        if (teamOrganizationList.size() == 0) {
+            TeamOrganization teamOrganization = new TeamOrganization();
+            teamOrganization.setHigherUser(curUser);
+            teamOrganization.setLowerUser(user);
+            teamOrganization.setHigherUser(recommend_man);
+            curUser.getLowerList().add(teamOrganization);
+        }
+        curUser.getServiceOrderList().add(order);
+        order.getHigherUserList().add(curUser);
+        userRepository.save(curUser);
+        List<TeamOrganization> userList = curUser.getHigherUserList();
+        for (TeamOrganization higherUser : userList) {
+            updateHigherLevel(higherUser.getHigherUser(), user, product, order, ++level);
+        }
+    }
+
+    /**
+     * 计算团队人员成分
+     *
+     * @param user
+     * @param product
+     * @param sendUser
+     */
+    private void calTeamAnalysis(User user, Product product, User sendUser) {
+        if (user.getRoleType() == RoleType.天使) {
+            sendUser.getTeamAnalysis().setAngle(sendUser.getTeamAnalysis().getAngle() - 1);
+        }
+
+        if (user.getRoleType() == RoleType.合伙人) {
+            sendUser.getTeamAnalysis().setPartner(sendUser.getTeamAnalysis().getPartner() - 1);
+        }
+
+        if (user.getRoleType() == RoleType.准合伙人) {
+            sendUser.getTeamAnalysis().setQuasiPartner(sendUser.getTeamAnalysis().getQuasiPartner() - 1);
+        }
+
+        if (user.getRoleType() == RoleType.高级合伙人) {
+            sendUser.getTeamAnalysis().setSeniorPartner(sendUser.getTeamAnalysis().getSeniorPartner() - 1);
+        }
+
+        if (product.getRoleType() == RoleType.天使) {
+            sendUser.getTeamAnalysis().setAngle(sendUser.getTeamAnalysis().getAngle() + 1);
+        }
+
+        if (product.getRoleType() == RoleType.合伙人) {
+            sendUser.getTeamAnalysis().setPartner(sendUser.getTeamAnalysis().getPartner() + 1);
+        }
+
+        if (product.getRoleType() == RoleType.准合伙人) {
+            sendUser.getTeamAnalysis().setQuasiPartner(sendUser.getTeamAnalysis().getQuasiPartner() + 1);
+        }
+
+        if (product.getRoleType() == RoleType.高级合伙人) {
+            sendUser.getTeamAnalysis().setSeniorPartner(sendUser.getTeamAnalysis().getSeniorPartner() + 1);
+        }
+    }
+}
