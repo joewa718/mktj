c78b9abee81d58051b413cbda9d461ca8a57fa0c
Wang Zhan <Wang.Zhan.ap@nielsen.com>
2017-09-24 22:24:01 +0800

    commit

--- src/main/java/com/mktj/cn/web/configuration/TransactionManagementConfig.java
+++ src/main/java/com/mktj/cn/web/configuration/TransactionManagementConfig.java
@@ -14,7 +14,7 @@ import javax.sql.DataSource;
 @Configuration
 public class TransactionManagementConfig {
 
-    @Bean
+    @Bean(value = "transactionManager")
     public PlatformTransactionManager transactionManager(@Qualifier("dataSource") @Autowired DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
